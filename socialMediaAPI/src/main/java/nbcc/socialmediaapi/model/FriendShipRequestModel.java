8216eac90e9ebf1dcabb2b5f24ca5d1afe3f72e7
Herbert Parasio <herbert.parasio@gmail.com>
2024-07-14 22:21:28 -0300

    Folder Structure Created

---
+++ socialMediaAPI/src/main/java/nbcc/socialmediaapi/model/FriendShipRequestModel.java
@@ -0,0 +1,5 @@
+package nbcc.socialmediaapi.model;
+
+public class FriendShipRequestModel {
+   
+}
---
+++ socialMediaAPI/src/main/java/nbcc/socialmediaapi/model/UserModel.java
@@ -0,0 +1,6 @@
+package nbcc.socialmediaapi.model;
+
+public class UserModel {
+    private String username;
+
+}
